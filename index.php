b5497754d9f0f0f5ffa219439b8c7361fd4aab14
linasmk <linas.mackonis@gmail.com>
2019-09-05 22:10:40 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,125 @@
+<?php include("includes/header.php"); ?>
+<article id="hero" class="hero">
+	<div class="hero_inner__wrapper">
+		<section class="hero_text fade_in">
+			<h1>Linas Mackonis <br><span>- front-end web developer</span></h1>
+		</section>
+		<div class="hero_img fade_in"></div>
+		<div class="iphone_img">
+			<a class="slide_out" href="#projects">
+			<img src="img/iphone_on.png" alt="Iphone with a turned on screen"></a>
+		</div>
+	</div>
+</article>
+<article id="about_me" class="about_me">
+	<h2 class="fade_in">About Me</h2>
+	<p class="fade_in">There are no preliminary data on why I started web development a little bit later, in my 30s, but most likely I was afraid of <a class="link" href="https://www.reddit.com/r/geek/comments/173dcn/internet_explorer_vs_murder_rate/" target="_blank">Internet Explorer</a>.</p>
+	<p class="fade_in">There are also no explicit estimates on how many times I crashed in front of my computer, while trying to do things in a proper way. However web development gradually turned into my working time, leisure time and even into my way of thinking.</p>
+	<p class="fade_in">I have recently graduated from <a class="link" href="https://www.baaa.dk/programmes/ap-degree/multimedia-design/" target="_blank">Business Academy Aarhus</a> as a fresh front-end developer. For the past several years I lived and continue my living in Aarhus, Denmark. This website is my call to action for the prospective employers around Aarhus or across Denmark.</p>
+	<p class="fade_in">If the link to my website somehow appeared in you mailbox, it means I have a particular interest in working at you company.</p>
+	<p class="fade_in">You are always welcome to contact me or check my <a href="https://github.com/linasmk">GitHub profile</a>. My page is being updated constantly and if there is a mismatch between the obtained and required skills, you are welcome to come back later and check it again.</p>
+</article>
+<article id="my_stack" class="my_stack">
+	<h2 class="fade_in">My Stack</h2>
+	<div class="my_stack__icons">
+		<img data-src="img/html_icon.svg" alt="HTML icon" class="html_icon fade_in" title="HTML">
+		<img data-src="img/css_icon.svg" alt="CSS icon" class="css_icon fade_in" title="CSS">
+		<img data-src="img/js_icon.svg" alt="JavaScript icon" class="css_icon fade_in" title="JavaScript">
+		<img data-src="img/sass_icon.svg" alt="Sass icon" class="sass_icon fade_in" title="SASS">
+		<img data-src="img/npm_icon.svg" alt="NPM icon" class="npm_icon fade_in" title="NPM">
+		<img data-src="img/gulp_icon.svg" alt="Gulp icon" class="gulp_icon fade_in" title="Gulp">
+		<img data-src="img/ai_icon.svg" alt="AI icon" class="ai_icon fade_in" title="Adobe Illustrator">
+		<img data-src="img/php_icon.svg" alt="PHP icon" class="php_icon fade_in" title="PHP">
+		<img data-src="img/wp_icon.svg" alt="WordPress icon" class="wp_icon fade_in" title="WordPress">
+		<img data-src="img/svg_icon.svg" alt="SVG icon" class="svg_icon fade_in" title="SVG">
+	</div>
+
+</article>
+<article id="projects" class="projects"> 
+	<section class="projects_content">
+		<h2 class="fade_in">Projects</h2>
+		<p class="fade_in">My designes, code structures and front-end applications are primarily applied to the WordPress Content Management system. This helps me to achieve goals, I wouldn't be capable to achieve without a firm back-end knowledge, which is a well-rounded and complete website.</p>
+
+		<p class="fade_in">WordPress gives me opportunities to understand the nuts and bolts of website structure, coding practises and workings of SEO. I try to develope my own themes with a minimal use of plugins, by implementing clean code. I try to avoid premium themes and I never use multi-puspose page buiders.</p>
+
+		<p class="fade_in">My workflow of building a WordPress site consists of kick starting a Gulp file, preprocessing SCSS to CSS, minifying JS files, making sure technical SEO is up to date and website's loading speed is optimized.</p>
+	</section>
+	<div class="project_background">
+		<div class="carousel">
+			<div class="slider">
+
+				<div class="slide">
+					<section class="slide_content">
+						<section class="project_description">
+							<h3 class="fade_in">Etno Dvaras</h3>
+							<p class="fade_in">Granny theme application for the Lithuanian restaurants chain Etno Dvaras.</p>
+						</section>
+						
+						<img class="mobile_screen fade_in" data-src="img/smartphone_etnodvaras.png" alt="Etnodvaras: mobile screen">
+						<img class="tablet_screen fade_in" data-src="img/tablet_etnodvaras.png" alt="Etnodvaras: tablet screen">
+						<img class="desktop_screen fade_in" data-src="img/laptop_etnodvaras.png" alt="Etnodvaras: desktop screen">
+					</section>
+					<a href="https://etnodvaras.lt/" class="project_link" target="_blank">View project</a>
+				</div><!-- End of slide -->
+
+				<div class="slide">
+					<div class="slide_content">
+						<section class="project_description">
+							<h3 class="fade_in">Art of Ink</h3>
+							<p class="fade_in">A presentational website's prototype for the Tattoo shop located in Aarhus, Denmark.</p>
+						</section>
+						
+						<img class="mobile_screen fade_in" data-src="img/smartphone_artofink.png" alt="Art of Ink: mobile screen">
+						<img class="tablet_screen fade_in" data-src="img/tablet_artofink.png" alt="Art of Ink: tablet screen">
+						<img class="desktop_screen fade_in" data-src="img/laptop_artofink.png" alt="Art of Ink: desktop screen">
+					</div>
+					<a href="http://mackonis.dk/art_of_ink/" class="project_link" target="_blank">View project</a>
+				</div><!-- End of slide -->
+
+				<div class="slide">
+					<div class="slide_content">
+						<section class="project_description">
+							<h3 class="fade_in">Madu</h3>
+							<p class="fade_in">My own developed WP restaurant theme dedicated to the Madu pub, located in the centre of Vilnius.</p>
+						</section>
+						
+						<img class="mobile_screen fade_in" data-src="img/smartphone_madu.png" alt="Madu: mobile screen">
+						<img class="tablet_screen fade_in" data-src="img/tablet_madu.png" alt="Madu: tablet screen">
+						<img class="desktop_screen fade_in" data-src="img/laptop_madu.png" alt="Madu: desktop screen">
+					</div>
+					<a href="http://www.bravenewwebsites.com" class="project_link" target="_blank">View project</a>
+				</div><!-- End of slide -->
+
+			</div><!-- End of slider -->
+			<div class="controls">
+				<span class="arrow left"><img src="img/left_arrow.png" alt="Left arrow"></span>
+				<span class="arrow right"><img src="img/right_arrow.png" alt="Right arrow"></span>
+				<ul class="slide_indicators">
+					<li class="selected"></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+
+		</div><!-- End of carousel -->
+    </div><!-- End of project_background -->
+</article>
+
+
+
+<article id="contact" class="contact">
+	<section class="contact_content">
+		<h2 class="fade_in">Contact</h2>
+		<p class="fade_in">You should definitely contact me if you are an employer seeking for a junior front-end developer with a growing WordPress and SEO skillset under his belt. Currently, I try to freelance and sharpen my skills as much as possible. Thus, you are also welcome to contact me if you have any ad hoc projects or if you need any help in making fast loading and neatly written WordPress site for your business.</p>
+	</section>
+	<div class="contact_background">
+		<section class="contact_details fade_in">
+			<a href="tel:+45-42-33-20-88" rel="nofollow"><span class="danish_flag"></span>+45 42 33 20 88</a>
+			<a href="tel:+370-672-48029" rel="nofollow"><span class="lithuanian_flag"></span>+370 672 48029</a>
+			<a href="mailto:linas.mackonis@gmail.com"><span class="email"></span>Email Me</a>  
+		</section>
+	</div>
+</article>
+
+
+<?php include("includes/footer.php"); ?>
